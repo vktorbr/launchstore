f452d88effdc892defb6e969effc2a56703f453d
Victor Martins <vms5@cin.ufpe.br>
2021-11-12 22:35:24 -0300

    implementacao da funcao de atualizacao dos dados dos produtos

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,10 @@ routes.get('/', function(req, res){
 })
 
 routes.get("/products/create", ProductController.create);
+routes.get("/products/:id/edit", ProductController.edit);
+
 routes.post("/products", ProductController.post);
+routes.put("/products", ProductController.put);
 
 routes.get("/ads/create", function(req, res){
     return res.redirect("/products/create");
